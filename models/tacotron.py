a0af25b0e669fe44cb771d9cf82576b3524ad222
cschafer26 <c.schaefer.home@gmail.com>
2022-04-29 08:30:37 +0200

    Repl LSA.

--- models/tacotron.py
+++ models/tacotron.py
@@ -109,7 +109,7 @@ class Decoder(nn.Module):
         self.register_buffer('r', torch.tensor(1, dtype=torch.int))
         self.n_mels = n_mels
         self.prenet = PreNet(n_mels)
-        self.attn_net = LSA(decoder_dims)
+        self.attn_net = Attention(decoder_dims)
         self.attn_rnn = nn.GRUCell(decoder_dims + decoder_dims // 2, decoder_dims)
         self.rnn_input = nn.Linear(2 * decoder_dims, lstm_dims)
         self.res_rnn1 = nn.LSTMCell(lstm_dims, lstm_dims)
