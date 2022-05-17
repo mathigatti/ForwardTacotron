2a6375dc9b255733efde3f5856a266997b6ba6f3
cschafer26 <c.schaefer.home@gmail.com>
2022-05-17 08:46:20 +0200

    Minor refactoring.

--- utils/dataset.py
+++ utils/dataset.py
@@ -261,9 +261,9 @@ class ForwardDataset(Dataset):
         x = self.tokenizer(text)
         mel = np.load(str(self.path/'mel'/f'{item_id}.npy'))
         mel_len = mel.shape[-1]
-        dur = np.load(str(self.path/'alg'/f'{item_id}.npy'))
+        dur = np.ones(len(x))#np.load(str(self.path/'alg'/f'{item_id}.npy'))
         pitch = np.load(str(self.path/'raw_pitch'/f'{item_id}.npy'))
-        energy = np.load(str(self.path/'phon_energy'/f'{item_id}.npy'))
+        energy = np.ones(len(x))
         return {'x': x, 'mel': mel, 'item_id': item_id, 'x_len': len(x),
                 'mel_len': mel_len, 'dur': dur, 'pitch': pitch, 'energy': energy}
 
